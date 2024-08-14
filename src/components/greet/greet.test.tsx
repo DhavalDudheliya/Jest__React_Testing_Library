d9ed611f9b20cd9fed45ebca906abd1b26316b28
Dhaval Dudheliya <dhavaldudheliya77@gmail.com>
2024-08-14 15:59:53 +0530

    learn file name formates, alternative of test keyword, describr function

--- src/components/greet/greet.test.tsx
+++ src/components/greet/greet.test.tsx
@@ -1,14 +1,18 @@
 import { render, screen } from "@testing-library/react";
 import { Greet } from "./greet";
 
-test("Greet renders correctly", () => {
-  render(<Greet />);
-  const textElement = screen.getByText(/hello/i);
-  expect(textElement).toBeInTheDocument();
-});
+describe("Greet", () => {
+  test("renders correctly", () => {
+    render(<Greet />);
+    const textElement = screen.getByText(/hello/i);
+    expect(textElement).toBeInTheDocument();
+  });
 
-test("Greet renders with a name", () => {
-  render(<Greet name="Dhaval" />);
-  const textElement = screen.getByText("Hello Dhaval");
-  expect(textElement).toBeInTheDocument();
+  describe("Nested", () => {
+    test("renders a name", () => {
+      render(<Greet name="Dhaval" />);
+      const textElement = screen.getByText("Hello Dhaval");
+      expect(textElement).toBeInTheDocument();
+    })
+  });
 });
