4919afae2b2da92453a609f61ff814bb21695a82
Dhaval Dudheliya <dhavaldudheliya77@gmail.com>
2024-08-15 10:45:27 +0530

    skills

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,14 @@ import logo from "./logo.svg";
 import "./App.css";
 import { Greet } from "./components/greet/greet";
 import { Application } from "./components/application/Application";
+import { Skills } from "./components/skills/skills";
 
 function App() {
   return (
     <div className="App">
       <Greet />
       <Application />
+      <Skills skills={["HTML", "CSS"]} />
     </div>
   );
 }
---
+++ src/components/skills/skills.test.tsx
@@ -0,0 +1,18 @@
+import { render, screen } from "@testing-library/react";
+import { Skills } from "./skills";
+
+describe("Skills", () => {
+  const skills = ["HTML", "CSS", "JavaScript"];
+
+  test("render correctly", () => {
+    render(<Skills skills={skills} />);
+    const listElement = screen.getByRole("list");
+    expect(listElement).toBeInTheDocument();
+  });
+
+  test("render a list of skills", () => {
+    render(<Skills skills={skills} />);
+    const listItemElements = screen.getAllByRole("listitem");
+    expect(listItemElements).toHaveLength(skills.length);
+  });
+});
