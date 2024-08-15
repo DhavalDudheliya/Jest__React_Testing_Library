873a0722278235f0dbe0224af6eacb1ed1d58c0a
Dhaval Dudheliya <dhavaldudheliya77@gmail.com>
2024-08-15 11:58:10 +0530

    skills finish

--- src/components/skills/skills.test.tsx
+++ src/components/skills/skills.test.tsx
@@ -1,4 +1,4 @@
-import { render, screen } from "@testing-library/react";
+import { logRoles, render, screen } from "@testing-library/react";
 import { Skills } from "./skills";
 
 describe("Skills", () => {
@@ -14,5 +14,38 @@ describe("Skills", () => {
     render(<Skills skills={skills} />);
     const listItemElements = screen.getAllByRole("listitem");
     expect(listItemElements).toHaveLength(skills.length);
+  });
+
+  test("renders login button", () => {
+    render(<Skills skills={skills} />);
+    const loginButton = screen.getByRole("button", {
+      name: "Login",
+    });
+    expect(loginButton).toBeInTheDocument();
+  });
+
+  //! queryByRole is return null if the element is not presents in the DOM. It is usefull when we wants to assert element not present in DOM.
+  test("not renders start lerning button", () => {
+    render(<Skills skills={skills} />);
+    const startLearningButton = screen.queryByRole("button", {
+      name: "Start learning",
+    });
+    expect(startLearningButton).not.toBeInTheDocument();
+  });
+
+  //! findBy return a promise after a default 1000ms if the the element found.
+  test("Start learning button is eventually displayed", async () => {
+    const view = render(<Skills skills={skills} />);
+    logRoles(view.container);
+    // screen.debug();
+    const startLearningButton = await screen.findByRole(
+      "button",
+      {
+        name: "Start learning",
+      },
+      { timeout: 2000 }
+    );
+    // screen.debug();
+    expect(startLearningButton).toBeInTheDocument();
   });
 });
